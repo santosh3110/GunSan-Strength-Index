763a27f345adc0a4b7c195cd4efe9a3ff308cb85
santoshkumarguntupalli <santoshkumarguntupalli@gmail.com>
2025-06-07 23:04:11 +0100

    instalation error cleared in version 0.1.1

--- setup.py
+++ setup.py
@@ -4,21 +4,13 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-# Helper to read requirements.txt
-def get_requirements(file_path: str) -> list[str]:
-    with open(file_path, "r") as file_obj:
-        requirements = [line.strip() for line in file_obj if line.strip() and line.strip() != "-e ."]
-    return requirements
-
 # Metadata
-__version__ = "0.1.0"
+__version__ = "0.1.1"  # updated addressing the installation error
 REPO_NAME = "GunSan-Strength-Index"
 AUTHOR_USER_NAME = "santosh3110"
 SRC_REPO = "gunsan_strength"
 AUTHOR_EMAIL = "santoshkumarguntupalli@gmail.com"
-LIST_OF_REQUIREMENTS = get_requirements("requirements.txt")
 
-# Setup
 setup(
     name=SRC_REPO,
     version=__version__,
@@ -28,13 +20,18 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=LIST_OF_REQUIREMENTS,
-    classifiers=[
-    "License :: OSI Approved :: Apache Software License",
-    "Programming Language :: Python :: 3",
-    "Operating System :: OS Independent",
+    install_requires=[
+        "pandas",
+        "numpy",
+        "vectorbt",
+        "pandas-ta",
+        "plotly"
     ],
-
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.7",
     include_package_data=True,
 )
